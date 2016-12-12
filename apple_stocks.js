b046489c144371df538d5ad89a8bbc16d61d2373
Michael Loschiavo <losch103@gmail.com>
2016-12-12 11:40:30 -0800

    Complete challenge. O(n^2) complexity

---
+++ apple_stocks.js
@@ -0,0 +1,33 @@
+// Writing programming interview questions hasn't made me rich. Maybe trading Apple stocks will.
+// Suppose we could access yesterday's stock prices as an array, where:
+
+// The indices are the time in minutes past trade opening time, which was 9:30am local time.
+// The values are the price in dollars of Apple stock at that time.
+// So if the stock cost $500 at 10:30am, stockPricesYesterday[60] = 500;.
+
+// Write an efficient function that takes stockPricesYesterday and returns the best profit I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday.
+
+// For example:
+
+// var stockPricesYesterday = [10, 7, 5, 8, 11, 9];
+
+// getMaxProfit(stockPricesYesterday); returns 6 (buying for $5 and selling for $11)
+// No "shorting"—you must buy before you sell. You may not buy and sell in the same time step (at least 1 minute must pass).
+
+
+const getMaxProfit = (stockPrices) => {
+  let profit = stockPrices[0] - stockPrices[1]
+
+  for (var i = 0; i < stockPrices.length - 1; i++) {
+    for (var j = i + 1; j < stockPrices.length; j++) {
+      let newProfit = stockPrices[j] - stockPrices[i]
+      if (newProfit > profit) profit =  newProfit
+    }
+  }
+  
+  return profit
+}
+
+
+let stockPricesYesterday = [10, 7, 5, 8, 11, 9]
+console.log(getMaxProfit(stockPricesYesterday))
